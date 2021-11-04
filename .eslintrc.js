49dfeed611b98b7e369f7527b83ae791ec9886e4
Francesco Novy <francesco@fabscale.com>
2021-11-04 09:00:11 +0100

    chore: Update eslint setup to use fabscale-eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,4 @@
 module.exports = {
   root: true,
-
-  env: {
-    browser: false,
-    node: true,
-  },
-
-  plugins: ['node'],
-
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:node/recommended',
-  ],
-
-  rules: {},
-
-  overrides: [],
+  ...require('fabscale-eslint-config/lib/node'),
 };
